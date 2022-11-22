ea7b3eb50af3206fb660a695e7f1bb29b00e88e4
julien heng <hengjchan@gmail.com>
2022-11-22 14:50:38 +0800

    finished second time

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import Typical from "react-typical";
 function Home() {
   return (
     <div name="// home" className="h-screen w-full flex flex-col justify-center md:flex md:flex-row md:items-center bg-gradient-to-b from-black to-gray-800">
-      <div className="text-white max-w-screen-lg mx-auto md:mr-5 w-fit flex flex-col justify-center my-3 border-solid border-2 border-red-700">
+      <div className="text-white max-w-screen-lg mx-auto md:mr-5 w-fit flex flex-col justify-center my-3">
         <h2 className="text-left text-3xl lg:text-4xl">Hey, it's...</h2>
         <h1 className="name w-full text-5xl my-6 md:text-8xl lg:mx-25 pb-2 tracking-tighter">JULIEN HENG</h1>
         <p className="job text-2xl md:text-5xl text-center mb-5">
@@ -14,7 +14,7 @@ function Home() {
         </p>
       </div>
 
-      <img src={profile} alt="my profile" className="w-2/5 md:w-4/12 md:flex md:justify-center mx-auto md:ml-5 rounded-full md:rounded-tl-[30%] md:rounded-tr-[70%] md:rounded-bl-[40%] md:rounded-br-[20%] shadow hover:shadow-white order-solid border-2 border-green-7" />
+      <img src={profile} alt="my profile" className="w-2/5 md:w-4/12 md:flex md:justify-center mx-auto md:ml-5 rounded-full md:rounded-tl-[30%] md:rounded-tr-[70%] md:rounded-bl-[40%] md:rounded-br-[20%] shadow hover:shadow-white" />
 
       <section id="section10" className="w-fit h-fit flex flex-col text-white absolute left-[44%] md:left-[50%] bottom-10">
         <Link to="// about" smooth duration={1000}>
