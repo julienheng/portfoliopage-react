496ee82cb970e1e3ca44a311f1d93e3a0f16b17d
julien heng <hengjchan@gmail.com>
2022-11-22 10:58:19 +0800

    updating

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ import Typical from "react-typical";
 
 function Home() {
   return (
-    <div name="// home" className="h-screen w-full flex flex-col justify-center md:flex items-center bg-gradient-to-b from-black to-gray-800">
+    <div name="// home" className="h-screen w-full flex flex-col justify-center md:flex md:flex-row md:items-center bg-gradient-to-b from-black to-gray-800">
       {/* <div className="max-w-screen-xl mx-auto flex flex-col items-center px-4 md:flex-row h-full relative"> */}
-      <div className="text-white max-w-screen-lg my-10 px-10 border-solid border-2 border-white-600">
+      <div className="text-white max-w-screen-lg flex flex-col justify-center my-3 mx-auto px-10 border-solid border-2 border-white-600">
         <h2 className="text-left text-3xl lg:text-4xl">Hey, it's...</h2>
         <h1 className="name w-full text-center text-5xl my-6 md:text-8xl lg:mx-25 pb-2 tracking-tighter">JULIEN HENG</h1>
         <p className="job text-2xl md:text-5xl text-center mb-5">
@@ -15,9 +15,9 @@ function Home() {
         </p>
       </div>
 
-      <img src={profile} alt="my profile" className="w-2/5 md:w-2/5 mx-auto rounded-full md:mx-auto md:rounded-tl-[30%] md:rounded-tr-[70%] md:rounded-bl-[40%] md:rounded-br-[20%] shadow hover:shadow-white border-solid border-2 border-green-600" />
+      <img src={profile} alt="my profile" className="w-2/5 md:w-3/12 md:flex md:justify-center rounded-full md:mx-auto md:rounded-tl-[30%] md:rounded-tr-[70%] md:rounded-bl-[40%] md:rounded-br-[20%] shadow hover:shadow-white" />
 
-      <section id="section10" className="w-fit h-fit flex flex-col text-white absolute left-[45%] md:left-[50%] bottom-10">
+      <section id="section10" className="w-fit h-fit flex flex-col text-white absolute left-[44%] md:left-[50%] bottom-10">
         <Link to="// about" smooth duration={1000}>
           <span></span>Scroll
         </Link>
