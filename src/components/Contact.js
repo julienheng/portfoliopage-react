a93867e475cee1486763d3947dfd7eb346785564
julien heng <hengjchan@gmail.com>
2022-11-22 12:35:35 +0800

    updating ;

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ function Contact() {
   return (
     <>
       <div name="// contact" className="w-full h-screen px-5 bg-gradient-to-b from-black to-gray-800 text-white border-2 border-solid border-green-700">
-        <div className="flex flex-col justify-center text-center max-w-screen-lg h-full">
-          <div className="">
+        <div className="flex flex-col justify-center text-center max-w-screen-lg h-full border-2 border-solid border-blue-700">
+          <div className="border-solid border-2 border-yellow">
             <p className="text-3xl md:text-5xl font-bold inline border-b-4 border-pink-500">Get To Know Me!</p>
             <p className="mt-3 text-md md:text-2xl">
               I am open to <span className="text-[#39C600]">Job</span> opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience please don't hesitate to <span className="text-[#39C600]">contact</span> me.
@@ -18,7 +18,7 @@ function Contact() {
             <p className="pt-5">Send me a message!</p>
           </div>
 
-          <div className="flex justify-center border-2 border-solid border-red-700 items-center">
+          <div className="flex flex-col justify-center border-2 border-solid border-red-700 items-center">
             <form action="" className="flex flex-col w-full">
               <input type="text" name="name" placeholder="Enter your name" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
 
